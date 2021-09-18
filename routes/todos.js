3d0812dcc0dcb7e4f146ffe4f11b4be9eb246741
jiniusz <dev.jiniusz@gmail.com>
2021-09-18 11:27:36 +0700

    third test pass

--- routes/todos.js
+++ routes/todos.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var createError = require('http-errors');
 var router = express.Router();
 
 const todos = [{ id: 1, name: 'Do something', completed: false }]
@@ -10,6 +11,10 @@ router.get('/', function(req, res, next) {
 
 router.get('/:id', function(req, res, next) {
     const foundTodo = todos.find(todo => todo.id === Number(req.params.id))
+
+    if(!foundTodo){
+        return next(createError(404, 'Not Found'))
+    }
 
     res.json(foundTodo)
 })
