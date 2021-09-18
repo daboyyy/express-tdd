a42343a69371bd1d9e5e93cd81247ad689258424
jiniusz <dev.jiniusz@gmail.com>
2021-09-18 10:02:36 +0700

    initial app

---
+++ app.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const app = require('./app');
+
+describe('Todos API' , () => {
+    it('GET /todos --> array todos', () => {
+
+    });
+    it('GET /todos/id --> array todo by ID', () => {
+
+    });
+    it('POST /todos --> created todo', () => {
+
+    });
+    it('GET /todos --> validates request body', () => {
+
+    });
+})
